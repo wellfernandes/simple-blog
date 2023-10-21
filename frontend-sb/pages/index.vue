41b62a803758bf7dd9e9e958745168a1d7103a3a
Welliton Fernandes Leal <wellfernandes@me.com>
2023-10-20 22:20:18 -0300

    feat: set up blog homepage and added default icon

--- frontend-sb/pages/index.vue
+++ frontend-sb/pages/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <Tutorial/>
+ <div class="w-1/2 mx-auto my-20">
+   <h1 class="text-3xl font-semibold text-red-600">Blog Title</h1>
+   <article class="my-20">
+     <h2 class="text-5xl font-semibold mb-5">Article Title</h2>
+     <p class="mb-5">Article Summary</p>
+     <p class="text-red-500">Read More</p>
+   </article>
+ </div>
 </template>
 
 <script>
