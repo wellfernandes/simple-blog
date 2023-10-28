f3274166cacdbd4e31c73050c222199de8d8c764
Welliton Fernandes Leal <wellfernandes@me.com>
2023-10-27 22:21:04 -0300

    refactor: moved code to new post component

--- frontend-sb/pages/index.vue
+++ frontend-sb/pages/index.vue
@@ -1,24 +1,6 @@
 <template>
-  <div class="w-1/2 mx-auto my-20">
-    <h1 class="text-3xl font-semibold text-red-600">Blog Title</h1>
-    <article class="my-20">
-      <h2 class="text-5xl font-semibold mb-5">Article Title</h2>
-      <p class="mb-5">Article Summary</p>
-      <p class="text-red-500">Read More</p>
-    </article>
-  </div>
+  <post></post>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      title: 'Blog Title',
-      article: {
-        title: 'Article Title',
-        summary: 'Article Summary'
-      }
-    }
-  },
-}
 </script>
