267dea12e460e6210e07b12bdaad1b883a24e2b7
Welliton Fernandes Leal <wellfernandes@me.com>
2024-02-21 11:32:51 -0300

    feat: added get articles

--- frontend-sb/components/post.vue
+++ frontend-sb/components/post.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="w-1/2 mx-auto my-20">
-    <h1 class="text-3xl font-semibold text-red-600">Blog Title</h1>
+    <h1 class="text-3xl font-semibold text-red-600">{{ this.title }}</h1>
     <article class="my-20">
-      <h2 class="text-5xl font-semibold mb-5">Article Title</h2>
-      <p class="mb-5">Article Summary</p>
+      <h2 class="text-5xl font-semibold mb-5">{{ this.article.title }}</h2>
+      <p class="mb-5">{{ this.article.summary }}</p>
       <p class="text-red-500">Read More</p>
     </article>
   </div>
@@ -20,6 +20,20 @@ export default {
       }
     }
   },
+
+  methods: {
+    getArticle () {
+      this.$axios.$get('http://localhost:3000/articles/1')
+        .then(response => {
+          this.article = response.data
+        })
+        .catch(error => {
+          console.log(error)
+          alert.error(ERROR_CONSTANTS.GET_ARTICLE_ERROR);
+        })
+    },
+  },
+
 }
 </script>
 
